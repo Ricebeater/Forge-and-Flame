d29a7897082fd741205c8970bd8c72aa805f7722
JahmOsaka <74894438+JahmOsaka@users.noreply.github.com>
2026-03-28 19:08:55 +0700

    feat:prologueCutscene

---
+++ Assets/Script/Animation/CutsceneController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Playables;
+
+public class CutsceneController : MonoBehaviour
+{
+    [Header("Cutscene Settings")]
+    public PlayableDirector director;
+    public GameObject cutsceneCamera;
+
+    [Header("Player Settings")]
+    public GameObject playerController;
+
+    void OnEnable()
+    {
+        if (director != null)
+        {
+            director.stopped += OnCutsceneFinished;
+        }
+    }
+
+    void OnDisable()
+    {
+        if (director != null)
+        {
+            director.stopped -= OnCutsceneFinished;
+        }
+    }
+
+    void OnCutsceneFinished(PlayableDirector obj)
+    {
+        if (cutsceneCamera != null) cutsceneCamera.SetActive(false);
+
+        if (playerController != null) playerController.SetActive(true);
+
+        gameObject.SetActive(false); 
+    }
+}
